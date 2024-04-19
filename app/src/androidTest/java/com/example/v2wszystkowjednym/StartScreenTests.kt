fe85acdc3ae738e21722d8246d79d1e9013ac33f
agnieszka.szynal <agnieszka.szynal@farmaprom.pl>
2024-04-19 14:51:40 +0200

    start screen tests

---
+++ app/src/androidTest/java/com/example/v2wszystkowjednym/StartScreenTests.kt
@@ -0,0 +1,51 @@
+package com.example.v2wszystkowjednym
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Rule
+import org.junit.Test
+
+class StartScreenTests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Test
+    fun isWelecomeTextVisible() {
+        composeTestRule.setContent {
+            StartScreen()
+        }
+
+        composeTestRule.onNodeWithText(context.getString(R.string.welcome_text)).assertIsDisplayed()
+    }
+
+    @Test
+    fun isChoiceTextVisible() {
+        composeTestRule.setContent {
+            StartScreen()
+        }
+
+        composeTestRule.onNodeWithText(context.getString(R.string.choice_text)).assertIsDisplayed()
+    }
+
+    @Test
+    fun isBmiButtonEnable() {
+        composeTestRule.setContent {
+            StartScreen()
+        }
+        composeTestRule.onNodeWithText(context.getString(R.string.bmi_button)).assertIsEnabled()
+    }
+
+    @Test
+    fun isMeasurementButtonEnable() {
+        composeTestRule.setContent {
+            StartScreen()
+        }
+        composeTestRule.onNodeWithText(context.getString(R.string.measurement_button)).assertIsEnabled()
+    }
+}
