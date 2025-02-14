551bbce8c28d38e0e2e0f20571ca198b6f9acd38
agnieszka.szynal <agnieszka.szynal@farmaprom.pl>
2025-02-14 20:11:17 +0100

    improving test on the main screen

--- app/src/androidTest/java/com/example/v2wszystkowjednym/StartScreenTests.kt
+++ app/src/androidTest/java/com/example/v2wszystkowjednym/StartScreenTests.kt
@@ -1,14 +1,16 @@
-/* package com.example.v2wszystkowjednym
+package com.example.v2wszystkowjednym
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -17,11 +19,15 @@ class StartScreenTests {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    private lateinit var navController: TestNavHostController
     val context = InstrumentationRegistry.getInstrumentation().targetContext
-    val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
 
-    @Test
-    fun isWelcomeTextVisible() {
+    @Before
+    fun setup() {
+        navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+
+        navController.navigatorProvider.addNavigator(ComposeNavigator())
+
         composeTestRule.setContent {
             NavHost(navController = navController, startDestination = "start") {
                 composable("start") {
@@ -29,43 +35,31 @@ class StartScreenTests {
                 }
             }
         }
-
-        composeTestRule.onNodeWithText(context.getString(R.string.welcome_text)).assertIsDisplayed()
     }
 
-    @Test
-    fun isChoiceTextVisible() {
-        composeTestRule.setContent {
-            NavHost(navController = navController, startDestination = "start") {
-                composable("start") {
-                    StartScreen(navController = navController)
-                }
+            @Test
+            fun isWelcomeTextVisible() {
+                composeTestRule.onNodeWithText(context.getString(R.string.welcome_text))
+                    .assertIsDisplayed()
+            }
+
+            @Test
+            fun isChoiceTextVisible() {
+                composeTestRule.onNodeWithText(context.getString(R.string.choice_text))
+                    .assertIsDisplayed()
+            }
+
+            @Test
+            fun isBmiButtonEnable() {
+                composeTestRule.onNodeWithText(context.getString(R.string.bmi_button))
+                    .assertIsEnabled()
+            }
+
+            @Test
+            fun isMeasurementButtonEnable() {
+                composeTestRule.onNodeWithText(context.getString(R.string.measurement_button))
+                    .assertIsEnabled()
             }
         }
-        composeTestRule.onNodeWithText(context.getString(R.string.choice_text)).assertIsDisplayed()
-    }
 
-    @Test
-    fun isBmiButtonEnable() {
-        composeTestRule.setContent {
-            NavHost(navController = navController, startDestination = "start") {
-                composable("start") {
-                    StartScreen(navController = navController)
-                }
-            }
-        }
-        composeTestRule.onNodeWithText(context.getString(R.string.bmi_button)).assertIsEnabled()
-    }
 
-    @Test
-    fun isMeasurementButtonEnable() {
-        composeTestRule.setContent {
-            NavHost(navController = navController, startDestination = "start") {
-                composable("start") {
-                    StartScreen(navController = navController)
-                }
-            }
-        }
-        composeTestRule.onNodeWithText(context.getString(R.string.measurement_button)).assertIsEnabled()
-    }
-} */
